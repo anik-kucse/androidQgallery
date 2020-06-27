c054f118a00ff0e4ca22e9006360bfdb5c7bc3df
J. M. Ashfiqur Rahman <anik.ku.cse@gmail.com>
2020-06-27 15:20:29 +0600

    Initial commit

---
+++ app/src/main/java/bd/rcr/androidqgallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package bd.rcr.androidqgallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
